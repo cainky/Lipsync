4810825bda9e0932ec9b357b48e561214e2bb502
Kyle Cain <kylecain0@gmail.com>
2023-09-22 02:33:06 -0400

    basically complete frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,26 @@ import { useEffect } from 'react';
 import Recorder from './components/Recorder';
 import { useState } from 'react';
 import axios from 'axios';
+import ProgressBar from './components/ProgressBar';
+
+const BACKEND_URL = "http://127.0.0.1:5000"
 
 function App() {
     const [activeRecorder, setActiveRecorder] = useState(null);
     const [audioURL, setAudioURL] = useState(null);
     const [videoURL, setVideoURL] = useState(null);
+    const [audioBlob, setAudioBlob] = useState(null);
+    const [videoBlob, setVideoBlob] = useState(null);
     const [mergedVideoURL, setMergedVideoURL] = useState(null);
 
+    const [audioSuccess, setAudioSuccess] = useState(false);
+    const [videoSuccess, setVideoSuccess] = useState(false);
+    const [mergeSuccess, setMergeSuccess] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [showProgressBar, setShowProgressBar] = useState(false);
+
+
 
     useEffect(() => {
         // Cleanup function
@@ -19,57 +31,112 @@ function App() {
         };
     }, [audioURL, videoURL]);
     
+    const [progressInterval, setProgressInterval] = useState(null);
+
+    const AVERAGE_TIME = 10000; // 10 seconds for example
+    const UPDATE_INTERVAL = 100; // update every 100ms
+    const MAX_SIMULATED_PROGRESS = 100; // The progress will stop at 100%
+
+    const simulateProgressBar = () => {
+        setShowProgressBar(true);
+        let elapsed = 0;
+        const interval = setInterval(() => {
+            elapsed += UPDATE_INTERVAL;
+            const currentProgress = parseFloat(((elapsed / AVERAGE_TIME) * 100).toFixed(2));
+            
+            // Stop updating progress at MAX_SIMULATED_PROGRESS
+            if (currentProgress >= MAX_SIMULATED_PROGRESS) {
+                clearInterval(interval);
+                setProgress(MAX_SIMULATED_PROGRESS);
+            } else {
+                setProgress(currentProgress);
+            }
+    
+        }, UPDATE_INTERVAL);
+        setProgressInterval(interval);
+    }
+    
+    useEffect(() => {
+        return () => {
+            if(progressInterval) clearInterval(progressInterval);
+        };
+    }, [progressInterval]);
+
+
     const handleMerge = async () => {
-        if (!audioURL || !videoURL) {
+        if (!audioBlob || !videoBlob) {
             setErrorMessage("Both audio and video recordings are required for merging.");
             return;
         }
-    
-        // Convert blob URLs to actual blobs
-        const audioBlob = await fetch(audioURL).then(res => res.blob());
-        const videoBlob = await fetch(videoURL).then(res => res.blob());
-    
+        simulateProgressBar();
+        setMergeSuccess(false);
+        
         const formData = new FormData();
         formData.append("audio", audioBlob, "audio.webm");
         formData.append("video", videoBlob, "video.webm");
-    
+        
+        console.log(audioBlob.size, videoBlob.size)
         try {
-            const response = await axios.post('/api/merge', formData, {
+            const response = await axios.post(BACKEND_URL+'/api/merge', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             });
             console.log("Backend Response:", response.data);
+            setMergedVideoURL(BACKEND_URL+response.data.videoPath);
+
+            if(progressInterval) clearInterval(progressInterval);  
+            setProgress(0);
+            setShowProgressBar(false);
+            setMergeSuccess(true);
     
-            setMergedVideoURL(response.data.videoPath);
             // Revoke the blob URLs after successful upload to free up memory
             URL.revokeObjectURL(audioURL);
             URL.revokeObjectURL(videoURL);
         } catch (error) {
             console.error("Error during merging:", error);
+            if(progressInterval) clearInterval(progressInterval);
+            setProgress(0);
+            setShowProgressBar(false);
             setErrorMessage("An error occurred during merging. Please try again.");
-        }
-    }
-
-    const handleStartRecording = (type) => {
-        setActiveRecorder(type);
+        }        
     };
     
 
-    const handleRecordingStop = (type, url) => {
+    const handleStartRecording = (type) => {
+        setProgress(0);
+        setMergeSuccess(false);
+        setActiveRecorder(type);
+        setAudioSuccess(false);
+        setVideoSuccess(false);
+        setMergedVideoURL(null);
+    };
+    
+
+    const handleStopRecording = (type, blob) => {
+        const blobURL = URL.createObjectURL(blob);
         if (type === 'audio') {
-            setAudioURL(url);
+            setAudioURL(blobURL);
+            setAudioBlob(blob);
+            setAudioSuccess(true);
+            setVideoSuccess(false);
         } else if (type === 'video') {
-            setVideoURL(url);
+            setVideoURL(blobURL);
+            setVideoBlob(blob);
+            setVideoSuccess(true);
+            setAudioSuccess(false);
         }
         setActiveRecorder(null);
     };
+    
+    
+    
 
     return (
         <div className="min-h-screen bg-darkBg text-darkText flex">
             {/* Side Menu */}
             <div className="w-1/5 min-w-xs bg-darkSidebar p-8 shadow-md">
-                <h1 className="text-2xl font-bold mb-6">Lipsync App</h1>
+                <h1 className="text-2xl font-bold mb-6">LipSync App</h1>
                 {/* Placeholder for menu items */}
                 <div>
                     <p className="text-gray-300 mb-4"></p>
@@ -83,20 +150,25 @@ function App() {
                     <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
                         <button 
                             onClick={() => {
+                                setActiveRecorder('audio');
                                 setErrorMessage(null);
                                 handleStartRecording('audio');
                             }}
                             className="bg-secondary hover:bg-secondary-hover text-white font-bold py-2 px-4 rounded"
+                            disabled={progress > 0 && progress < 100}
                         >
                             Audio Recording
                         </button>
 
+
                         <button 
                             onClick={() => {
+                                setActiveRecorder('video');
                                 setErrorMessage(null);
                                 handleStartRecording('video');
                             }}
                             className="bg-secondary hover:bg-secondary-hover text-white font-bold py-2 px-4 rounded"
+                            disabled={progress > 0 && progress < 100}
                         >
                             Video Recording
                         </button>
@@ -111,15 +183,24 @@ function App() {
                     {activeRecorder && 
                         <Recorder 
                             type={activeRecorder} 
-                            onRecordingStop={(url) => handleRecordingStop(activeRecorder, url)}
+                            onRecordingStop={(url) => handleStopRecording(activeRecorder, url)}
                         />
                     }
 
+                    {audioSuccess && <div className="mt-4 p-4 bg-green-600 text-white rounded">Audio Recorded Successfully!</div>}
+                    {videoSuccess && <div className="mt-4 p-4 bg-green-600 text-white rounded">Video Recorded Successfully!</div>}
+                    {mergeSuccess && 
+                        <div className="mt-4 p-4 bg-green-600 text-white rounded">
+                            Merging completed successfully!
+                        </div>
+                    }
                     {errorMessage && 
                         <div className="mt-4 p-4 bg-red-600 text-white rounded">
                             {errorMessage}
                         </div>
                     }
+
+                    {showProgressBar && <ProgressBar progress={progress} />}
 
                     {mergedVideoURL && 
                         <video src={mergedVideoURL} controls autoPlay className="mt-4 rounded w-full" />
--- frontend/src/components/Recorder.jsx
+++ frontend/src/components/Recorder.jsx
@@ -1,5 +1,4 @@
-import { useRef, useEffect } from 'react';
-import { useReactMediaRecorder } from 'react-media-recorder';
+import { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import RecordButton from './RecordButton';
@@ -7,48 +6,96 @@ import StatusDisplay from './StatusDisplay';
 import StopButton from './StopButton';
 
 function Recorder({ type, onRecordingStop }) {
-    const {
-        status,
-        startRecording,
-        stopRecording,
-        mediaBlobUrl,
-        previewStream
-    } = useReactMediaRecorder({ video: type !== 'audio', audio: true });
-
+    const [status, setStatus] = useState("idle");
     const videoRef = useRef(null);
+    const [mediaRecorder, setMediaRecorder] = useState(null);
+    const [chunks, setChunks] = useState([]);
+    const [recordedBlob, setRecordedBlob] = useState(null);
     
     useEffect(() => {
-        const currentVideoRef = videoRef.current;
-        if (currentVideoRef && previewStream) {
-            currentVideoRef.srcObject = previewStream;
+        if (recordedBlob) {
+            onRecordingStop(recordedBlob);
         }
-        return () => {
-            if (currentVideoRef) {
-                currentVideoRef.srcObject = null;
-            }
-        };
-    }, [previewStream]);
+    }, [recordedBlob, onRecordingStop]);
+    useEffect(() => {
+        const constraints = (type === 'video') 
+            ? { video: true, audio: true } 
+            : { audio: true };
+    
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(stream => {
+                const mimeType = type === 'video' ? 'video/webm' : 'audio/webm';
+                const recorder = new MediaRecorder(stream, { mimeType: mimeType });
+    
+                recorder.ondataavailable = event => {
+                    console.log("Data available with size:", event.data.size);
+                    setChunks(prev => [...prev, event.data]);
+                };
+    
+                recorder.onstop = () => {
+                    // Use functional update to get the latest chunks
+                    setChunks(prevChunks => {
+                        const blob = new Blob(prevChunks, { type: mimeType });
+                        setRecordedBlob(blob);
+                        return [];
+                    });
+                    setStatus("stopped");
+                };
+    
+                recorder.onerror = event => {
+                    console.error("MediaRecorder error:", event.error);
+                };
+    
+                setMediaRecorder(recorder);
+    
+                if (type === 'video') {
+                    videoRef.current.srcObject = stream;
+                }
+            })
+            .catch(error => {
+                console.error("Error accessing media devices:", error);
+            });
+    }, [type, onRecordingStop]);
+    
+
+    const handleStart = () => {
+        if (mediaRecorder) {
+            mediaRecorder.start();
+            setStatus("recording");
+        }
+    };
 
     const handleStop = () => {
-        if (onRecordingStop) {
-            onRecordingStop(mediaBlobUrl);
+        console.log("Stop function called. MediaRecorder state:", mediaRecorder?.state);
+        if (mediaRecorder && mediaRecorder.state !== "inactive") {
+            mediaRecorder.stop();
+            if (type === 'video' && videoRef.current && videoRef.current.srcObject) {
+                let stream = videoRef.current.srcObject;
+                let tracks = stream.getTracks();
+    
+                tracks.forEach(function(track) {
+                    track.stop();
+                });
+    
+                videoRef.current.srcObject = null;
+            }
         }
-        stopRecording();
     };
+    
 
     return (
         <div className="bg-darkContent p-5 rounded shadow-lg">
             <StatusDisplay status={status} type={type} />
-   
+
             <div className="flex space-x-4 mt-4">
-                <RecordButton type={type} onClick={startRecording} />
+                <RecordButton type={type} onClick={handleStart} />
                 <StopButton onClick={handleStop} />
             </div>
 
-            {type === 'video' && 
-                <video 
-                    ref={videoRef} 
-                    autoPlay muted 
+            {type === 'video' &&
+                <video
+                    ref={videoRef}
+                    autoPlay muted
                     className="mt-4 rounded w-full"
                 />
             }
