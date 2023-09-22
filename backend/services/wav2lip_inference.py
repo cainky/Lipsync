b432992a9b5f43b3788cd93183fd479ea6feee79
Kyle Cain <kylecain0@gmail.com>
2023-09-22 14:09:44 -0400

    add file conversion using ffmpeg

--- backend/services/wav2lip_inference.py
+++ backend/services/wav2lip_inference.py
@@ -6,29 +6,46 @@ SEGMENTATION_PATH = "wav2lip-hq/checkpoints/face_segmentation.pth"
 SR_PATH = "wav2lip-hq/checkpoints/esrgan_max.pth"
 
 
+def run_command(cmd):
+    result = subprocess.run(cmd, text=True)
+    if result.returncode != 0:
+        raise Exception(
+            f"Command {cmd[0]} failed with return code {result.returncode}."
+        )
+    return result
+
+
 def convert_webm_to_wav(input_path, output_path):
     cmd = ["ffmpeg", "-y", "-i", input_path, output_path]
-    result = subprocess.run(cmd, capture_output=True, text=True)
-
-    if result.returncode != 0:
-        raise Exception(f"FFmpeg conversion failed. Error: {result.stderr}")
+    run_command(cmd)
 
 
 def convert_to_wav(audio_path):
-    """
-    Ensure that the provided audio file is in .wav format.
-    If it's in .webm, it will be converted to .wav.
-    """
     if audio_path.endswith(".webm"):
         output_path = audio_path.replace(".webm", ".wav")
         convert_webm_to_wav(audio_path, output_path)
-        # os.remove(audio_path)
-        return output_path
+        if os.path.exists(output_path):
+            os.remove(audio_path)
+            return output_path
+        raise Exception(f"Failed to convert {audio_path} to .wav")
     return audio_path
+
+
+def convert_to_mp4(face_path):
+    if face_path.endswith(".webm"):
+        output_path = face_path.rsplit(".", 1)[0] + ".mp4"
+        cmd = ["ffmpeg", "-y", "-i", face_path, output_path]
+        run_command(cmd)
+        if os.path.exists(output_path):
+            os.remove(face_path)
+            return output_path
+        raise Exception(f"Failed to convert {face_path} to .mp4")
+    return face_path
 
 
 def run_wav2lip_inference(face, audio, outfile):
     audio = convert_to_wav(audio)
+    face = convert_to_mp4(face)
 
     # DEBUG
     print("Starting Wav2Lip inference...")
@@ -53,13 +70,10 @@ def run_wav2lip_inference(face, audio, outfile):
         SR_PATH,
     ]
 
-    result = subprocess.run(cmd, text=True)
+    result = run_command(cmd)
 
     if not os.path.exists(outfile):
         error_msg = result.stderr if result.stderr else result.stdout
         raise Exception(f"Output video not found after processing. Error: {error_msg}")
-    if result.returncode != 0:
-        error_msg = result.stderr if result.stderr else result.stdout
-        raise Exception(f"Wav2Lip processing failed. Error: {error_msg}")
 
     return outfile
