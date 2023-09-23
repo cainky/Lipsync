7042325f5c06a12cf9d9e1cea6534876daf33101
Kyle Cain <kylecain0@gmail.com>
2023-09-22 21:04:24 -0400

    update tests

--- backend/services/merge_service.py
+++ backend/services/merge_service.py
@@ -7,24 +7,34 @@ DEFAULT_VIDEO_FILENAME = "face.webm"
 DEFAULT_OUTPUT_FILENAME = "output.mp4"
 
 
-def merge_audio_video(audio_file, video_file):
+def save_files(audio_file, video_file):
     uploads_dir = get_uploads_dir()
     audio_path = os.path.join(uploads_dir, DEFAULT_AUDIO_FILENAME)
     video_path = os.path.join(uploads_dir, DEFAULT_VIDEO_FILENAME)
-    output_path = os.path.join(uploads_dir, DEFAULT_OUTPUT_FILENAME)
 
     audio_file.save(audio_path)
     video_file.save(video_path)
+
+    return audio_path, video_path
+
+
+def clean_files(audio_path, video_path):
+    if os.path.exists(audio_path):
+        os.remove(audio_path)
+    if os.path.exists(video_path):
+        os.remove(video_path)
+
+
+def merge_audio_video(audio_file, video_file):
+    audio_path, video_path = save_files(audio_file, video_file)
+    output_path = os.path.join(get_uploads_dir(), DEFAULT_OUTPUT_FILENAME)
 
     try:
         run_wav2lip_inference(
             face_path=video_path, audio_path=audio_path, outfile_path=output_path
         )
     except Exception as e:
-        if os.path.exists(audio_path):
-            os.remove(audio_path)
-        if os.path.exists(video_path):
-            os.remove(video_path)
+        clean_files(audio_path, video_path)
         raise Exception("Output video not found after processing. Error: " + str(e))
 
     return output_path
--- backend/tests/services/test_merge_service.py
+++ backend/tests/services/test_merge_service.py
@@ -6,25 +6,32 @@ from app import create_app
 
 
 class TestMergeService(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app()
-        self.ctx = self.app.app_context()
-        self.ctx.push()
+    @classmethod
+    def setUpClass(cls):
+        cls.app = create_app()
+        cls.ctx = cls.app.app_context()
+        cls.ctx.push()
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.ctx.pop()
+
+    def setUp(self):
         self.sample_audio_file = MagicMock(spec=["filename", "save"])
         self.sample_audio_file.filename = "test_audio.webm"
-
         self.sample_video_file = MagicMock(spec=["filename", "save"])
         self.sample_video_file.filename = "test_video.webm"
 
     def tearDown(self):
-        self.ctx.pop()
-        if os.path.exists("uploads/test_audio.webm"):
-            os.remove("uploads/test_audio.webm")
-        if os.path.exists("uploads/test_video.webm"):
-            os.remove("uploads/test_video.webm")
-        if os.path.exists("uploads/output.mp4"):
-            os.remove("uploads/output.mp4")
+        # Using a list of paths to make future additions easier
+        paths_to_remove = [
+            "uploads/test_audio.webm",
+            "uploads/test_video.webm",
+            "uploads/output.mp4",
+        ]
+        for path in paths_to_remove:
+            if os.path.exists(path):
+                os.remove(path)
 
     @patch("os.path.exists", return_value=True)
     @patch("os.remove")
@@ -36,11 +43,18 @@ class TestMergeService(unittest.TestCase):
             self.sample_audio_file, self.sample_video_file
         )
 
-        self.assertTrue(
-            "output.mp4" in output_path, "Expected output.mp4 in the output path"
+        self.assertIn(
+            "output.mp4", output_path, "The output path should contain 'output.mp4'"
         )
-        self.sample_audio_file.save.assert_called()
-        self.sample_video_file.save.assert_called()
+        expected_audio_path = os.path.join(
+            os.getcwd(), "uploads", merge_service.DEFAULT_AUDIO_FILENAME
+        )
+        self.sample_audio_file.save.assert_called_with(expected_audio_path)
+
+        expected_video_path = os.path.join(
+            os.getcwd(), "uploads", merge_service.DEFAULT_VIDEO_FILENAME
+        )
+        self.sample_video_file.save.assert_called_with(expected_video_path)
 
     @patch("os.path.exists", return_value=False)
     @patch("os.remove")
@@ -48,13 +62,24 @@ class TestMergeService(unittest.TestCase):
     def test_merge_audio_video_failure(self, mock_run, mock_remove, mock_exists):
         mock_run.return_value = MagicMock(returncode=1, stderr="Error in Wav2Lip")
 
-        with self.assertRaises(
-            Exception, msg="Expected exception for failed video processing"
-        ) as context:
+        with self.assertRaisesRegex(
+            Exception, "Output video not found after processing"
+        ):
             merge_service.merge_audio_video(
                 self.sample_audio_file, self.sample_video_file
             )
 
-        self.assertTrue(
-            "Output video not found after processing" in str(context.exception)
-        )
+    @patch("os.path.exists", return_value=False)
+    @patch("os.remove")
+    @patch("services.wav2lip_inference.run_command")
+    def test_merge_audio_video_with_invalid_audio_format(
+        self, mock_run_command, mock_remove, mock_exists
+    ):
+        mock_run_command.side_effect = Exception("Invalid audio format")
+        # Use a non-webm audio file to simulate an invalid format
+        self.sample_audio_file.filename = "test_audio.mp3"
+
+        with self.assertRaisesRegex(Exception, "Invalid audio format"):
+            merge_service.merge_audio_video(
+                self.sample_audio_file, self.sample_video_file
+            )
